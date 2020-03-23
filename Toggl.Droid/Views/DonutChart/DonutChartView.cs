25cf83a61a56acf641c54e88fa9b04ce7b3627de
Daivid S. Silverio <daivid.silverio@gmail.com>
2020-03-23 13:55:47 -0400

    :bug: Initialize slices with an empty collection by default

--- Toggl.Droid/Views/DonutChart/DonutChartView.cs
+++ Toggl.Droid/Views/DonutChart/DonutChartView.cs
@@ -4,6 +4,8 @@ using Android.Runtime;
 using Android.Util;
 using Android.Views;
 using System;
+using System.Collections.Immutable;
+using System.Linq;
 using Toggl.Core.UI.ViewModels.Reports;
 using Toggl.Droid.Extensions;
 using Toggl.Shared;
@@ -46,7 +48,7 @@ namespace Toggl.Droid.Views
         private int selectedSliceDurationTextSize;
 
         private bool isLoading;
-        private SlicesCollection slices;
+        private SlicesCollection slices = new SlicesCollection(ImmutableList<ReportDonutChartElement.PercentageDecoratedSegment>.Empty);
         private Slice selectedSlice;
 
         private SliceSelectionAnimator selectionAnimator;
